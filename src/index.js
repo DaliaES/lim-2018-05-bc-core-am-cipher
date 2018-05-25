f760543e471daf804318bf31de49327d05205d2e
DaliaES <dalia.escuderov@gmail.com>
2018-05-25 10:54:12 -0500

    avance html,js y css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-function btnCifrar(){
+//function btnCifrar(){
+    let btnCifrar = ()=>{
     document.getElementById("cipmsg").value = document.getElementById("plainmsg").value;
   }
-  btnCipher.addEventListener("click",btnCifrar)
-  function btnDescifrar(){
+  btnCipher.addEventListener("click",btnCifrar);
+  //function btnDescifrar(){
+    //let; btnDescifrar =()=>{
+   // document.getElementById("plainmsg").value = document.getElementById("cipmsg").value;
+  //}
+  //btnDeCipher.addEventListener("click",btntext)
+    let btndeCifrar = ()=>{
     document.getElementById("plainmsg").value = document.getElementById("cipmsg").value;
   }
-  btnDeCipher.addEventListener("click",btnDescifrar)
-  
+  btnDeCipher.addEventListener("click",btndeCifrar);
+  let btnborrar =()=>{
+   document.getElementById("cipmsg").value;
+  }
+  reset.addEventListener("click",btnborrar)
