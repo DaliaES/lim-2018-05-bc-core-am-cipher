07c9cf2eca6fe942f417c6becbc25f8a618be6f2
dalia <dalia.escuderov@gmail.com>
2018-05-31 22:41:25 -0500

    avance js

--- src/index.js
+++ src/index.js
@@ -1,44 +1,73 @@
 //fncion para cifrar el mensaje
-   
+let  encuentraAscii = (num) =>{
+  let encuentra = false;
+  for (let i = 97; i <= 122; i++) {
+      if (num == i) {
+          encuentra = true;
+      }
+  }
+  for (let i = 65; i <= 90; i++) {
+      if (num == i) {
+          encuentra = true;
+      }
+  }
+  return encuentra;
+};
+let nextAscii = (value, numero) =>{
+  let next = value + numero;
+   for (let i = 97; i <= 122; i++) {
+      if (i == value && next > 122) {
+          next = 96 + (next - 122);
+      }
+  }
+  for (let i = 65; i <= 90; i++) {
+      if (i == value && next > 90) {
+          next = 64 + (next - 90);
+      }
+  }
+  return String.fromCharCode(next);
+};
+let backAscii = (value, numero) => {
+  let back = value - numero;
+  ;
+  for (let i = 97; i <= 122; i++) {
+      if (i == value && back < 97) {
+          back = 123 - (97 - back);
+      }
+  }
+  for (let i = 65; i <= 90; i++) {
+      if (i == value && back < 65) {
+          back = 91 - (65 - back);
+      }
+  }
+  return String.fromCharCode(back);
+};
 let encode = (offset,string)=>{
   let msgcipher = "";
   let number = parseInt(offset);
   for ( let i=0;i<string.length;i++){
-    let asci = string.charCodeAt(i);
-    if (65 <= asci && asci <=  90){
-     let ecuaCif = ((asci - 65 + number)%26 + 65);
+    if (encuentraAscii(string.charCodeAt(i))) {
+        msgcipher += nextAscii(string.charCodeAt(i), number);
+      }  else {
+        msgcipher += String.fromCharCode(string.charCodeAt(i) + number);
+      }
     }
-      else  if (97 <= asci && asci <=  122){
-     ecuaCif = (asci - 97 + number)%26 + 97;
-     }
-        else {
-       ecuaCif = asci;  
-     }
-       msgcipher += String.fromCharCode(ecuaCif);
+  return msgcipher;
   }
-    return msgcipher;
- }
 //funcion para descifrar
  let decode = (offset,string) => {
   let msgdescipher = "";
   let number = parseInt(offset);
-  let i= 0;
   for (let i=0;i<string.length;i++){
-    let asci = string.charCodeAt(i);
-    if (65 <= asci && asci <=  90){
-     let ecuaDesCif = (asci - 65 - number)%26 + 65; 
-     msgdescipher += String.fromCharCode(ecuaDesCif);
-     } 
-     else  if (97 <= asci && asci <=  122){
-     ecuaDesCif = ((asci - 97 - number)%26 + 97);
-     }
-     else  {
-     ecuaDesCif = asci;   
-     }
-     msgdescipher += String.fromCharCode(ecuaDesCif);
+    if (encuentraAscii(string.charCodeAt(i))) {
+      msgdescipher += backAscii(string.charCodeAt(i), number);
   }
-    return msgdescipher;
- }
+  else {
+      msgdescipher += String.fromCharCode(string.charCodeAt(i) - number);
+  }
+}
+return msgdescipher;
+}
   //interaccion con el DOM
  //definir var offset y string
  const claveDespl = document.getElementById("key");
